d549bf817f57cb3527db47afbd7da810054e4716
a.breezy <ambrose.wilkins@gmail.com>
2022-04-06 10:35:00 -0400

    add state and useEffect to todo and todoList. also added localStorage to todo items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { use } from "bcrypt/promises";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 // component imports
@@ -6,16 +7,68 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+	// STATES
 	const [inputText, setInputText] = useState("");
 	const [todos, setTodos] = useState([]);
+	const [status, setStatus] = useState("all");
+	const [filteredTodos, setFilteredTodos] = useState([]);
+
+	// RUN ONCE WHEN APP STARTS
+	useEffect(() => {
+		getLocalTodos();
+	}, []);
+
+	// USE EFFECT
+	useEffect(() => {
+		filterHandler();
+		saveLocalTodos();
+	}, [todos, status]);
+
+	// FUNCTIONS
+	const filterHandler = () => {
+		switch (status) {
+			case "completed":
+				setFilteredTodos(todos.filter((todo) => todo.completed === true));
+				break;
+			case "uncompleted":
+				setFilteredTodos(todos.filter((todo) => todo.completed === false));
+				break;
+			default:
+				setFilteredTodos(todos);
+				break;
+		}
+	};
+
+	// save/get to local storage
+	const saveLocalTodos = () => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	};
+	const getLocalTodos = () => {
+		if (localStorage.getItem("todos") === null) {
+			localStorage.setItem("todos", JSON.stringify([]));
+		} else {
+			let todoLocal = JSON.parse(localStorage.getItem("todos"));
+			setTodos(todoLocal);
+		}
+	};
 
 	return (
 		<div className="App">
 			<header>
 				<h1>Ambrose's Todo List</h1>
 			</header>
-			<Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
-			<TodoList todos={todos} />
+			<Form
+				todos={todos}
+				setTodos={setTodos}
+				inputText={inputText}
+				setInputText={setInputText}
+				setStatus={setStatus}
+			/>
+			<TodoList
+				todos={todos}
+				setTodos={setTodos}
+				filteredTodos={filteredTodos}
+			/>
 		</div>
 	);
 }
