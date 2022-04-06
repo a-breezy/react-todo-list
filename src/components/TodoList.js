0e22bdf8ed76f5affe616efdfa473a847c604143
a.breezy <ambrose.wilkins@gmail.com>
2022-04-06 09:28:34 -0400

    add todo and logic to todoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const TodoList = () => {
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => {
 	return (
 		<div className="todo-container">
-			<ul class="todo-list"></ul>
+			<ul className="todo-list">
+				{todos.map((todo) => (
+					<Todo key={todo.id} text={todo.text} />
+				))}
+			</ul>
 		</div>
 	);
 };
